2eacea5ed2d418eda5993f1d4602f26f50ed691f
geekymv <ym2011678@foxmail.com>
2019-02-27 16:06:52 +0800

    添加说明

--- src/main/java/com/geekymv/designpatterns/proxy/cglib/CglibProxyTest.java
+++ src/main/java/com/geekymv/designpatterns/proxy/cglib/CglibProxyTest.java
@@ -1,5 +1,10 @@
 package com.geekymv.designpatterns.proxy.cglib;
 
+/**
+ * cglib 是针对类来实现的，它的原理是对指定的目标类生成一个子类，并覆盖其中的方法(非final的public类型)实现增强。
+ * 因为采用的是继承，所以不能对final修饰的类进行代理。
+ *
+ */
 public class CglibProxyTest {
 
     public static void main(String[] args) {
